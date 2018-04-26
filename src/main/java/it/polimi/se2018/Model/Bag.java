4c2a6e1447e77cf33e31ee6431771a5aa1a9711b
edoar <edoardolam@gmail.com>
2018-04-26 14:13:54 +0200

    Implement extractDie, insertDie, drawDice of Bag. Implement getter of Color
    Signed-off-by: edoar <edoardolam@gmail.com>

--- src/main/java/it/polimi/se2018/Model/Bag.java
+++ src/main/java/it/polimi/se2018/Model/Bag.java
@@ -2,11 +2,13 @@ package it.polimi.se2018.Model;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 public class Bag {
     private final ArrayList<Integer> remainingColors; //ogni elemento dell'arraylist corrisponde a un colore e indica quanti dati sono rimasti nella bag di quel colore
     private final int colorsNumber; //numero totale di colori che ci sono. (nel gioco è 5, facciamo così per renderlo estendibile)
-    private final int diceNumber; //numero totale di dadi che ci sono. (nel gioco è 90, facciamo così per renderlo estendibile)
+    private int diceNumber; //numero totale di dadi che ci sono. (nel gioco è 90, facciamo così per renderlo estendibile)
     private final int coloredDiceNumber; //numero di dati di uno specifico colore
 
     //gli passi il numero totale di colori e il numero totale di dadi e inizializza remainingColors.
@@ -26,15 +28,31 @@ public class Bag {
 
     //draws a single die from the bag, used by tool cards
     public Die extractDie () {
-        return null;
+        int randomDie = new Random().nextInt(diceNumber) + 1;
+        int index = 0;
+        for (int i = 0; remainingColors.get(i) < randomDie; i++) {
+            randomDie -= remainingColors.get(i);
+            index += 1;
+        }
+        remainingColors.set(index, remainingColors.get(index) - 1);
+        diceNumber -= 1;
+        int randomValueOfDie = new Random().nextInt(6) + 1;
+        Die die = new Die (randomValueOfDie, Color.fromInt(index));
+        return die;
     }
 
     public void insertDie (Die die) { //inserts a single die in the bag, used by tool cards
+        diceNumber += 1;
+        remainingColors.set(Color.fromColor(die.getColor()), remainingColors.get(Color.fromColor(die.getColor())) + 1);
     }
 
     //draws 2n+1 dice putting them in a arraylist, used by Board
     ArrayList<Die> drawDice (int playersNumber) {
-        return null; //se non avessimo scritto questo return, ci avrebbe dato errore e non ci avrebbe lasciato scrivere il prototipo del metodo
+        ArrayList<Die> drawDice = new ArrayList<Die>();
+        for (int i = 0; i < 2 * playersNumber + 1; i++) {
+            drawDice.add(extractDie());
+        }
+        return drawDice;
     }
 
 
--- src/main/java/it/polimi/se2018/Model/Color.java
+++ src/main/java/it/polimi/se2018/Model/Color.java
@@ -6,9 +6,28 @@ public enum Color {
 
     private Color(String abbreviation) {
     }
+    public static int fromColor (Color color) {
+        int index = 0;
+        for (Color col : Color.values()) {
+            if (col == color) break;
+            index += 1;
+        }
+        return index;
+    }
 
     public String getAbbreviation(){
         return abbreviation;
     }
-
+    public static Color fromInt(int value) {
+        Color color = null;
+        switch (value) {
+            case 0: color = Color.BLUE; break;
+            case 1: color = Color.RED; break;
+            case 2: color = Color.GREEN; break;
+            case 3: color = Color.YELLOW; break;
+            case 4: color = Color.PURPLE; break;
+            case 5: color = Color.WHITE; break;
+        }
+        return color;
+    }
 }
