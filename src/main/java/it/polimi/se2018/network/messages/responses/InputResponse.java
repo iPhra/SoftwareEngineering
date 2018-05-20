775c8b5866a74c090096b2e44b2f1c5a3db12b95
iPhra <francesco1.lorenzo@mail.polimi.it>
2018-05-20 19:00:19 +0200

    Update JavaDoc

--- src/main/java/it/polimi/se2018/network/messages/responses/InputResponse.java
+++ src/main/java/it/polimi/se2018/network/messages/responses/InputResponse.java
@@ -2,7 +2,13 @@ package it.polimi.se2018.network.messages.responses;
 
 import it.polimi.se2018.model.Color;
 
+/**
+ * This class represents a response from the Server to the Client containing the color extracted for a die
+ */
 public class InputResponse extends Response {
+    /**
+     * This is the color extracted
+     */
     private Color color;
 
     public InputResponse(int player, Color color) {
@@ -10,9 +16,16 @@ public class InputResponse extends Response {
         this.color = color;
     }
 
+    /**
+     * @return the color of the die
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Uses the handler to handle this specific input response
+     * @param responseHandler is the object who will handle this response
+     */
     public void handle(ResponseHandler responseHandler) { responseHandler.handleResponse(this);}
 }
